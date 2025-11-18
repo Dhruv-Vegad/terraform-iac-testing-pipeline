74fde162532088dd440e6d4037040f8e84198201
Dhruv Vegad <dhruvavegad@gmail.com>
2025-11-18 11:01:49 +0530

    Port Changed

--- main.tf
+++ main.tf
@@ -15,20 +15,9 @@ terraform{
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
 }
 
-variable "server_http_port" {
-    description = "The Port for the server"
-    type = number
-    default = 8080
-}
-
-variable "region" {
-  description = "region specified here"
-    type = string
-  default = "ap-south-1"
-}
 
 output "aws_region"{
     value = var.region
---
+++ variable.tf
@@ -0,0 +1,12 @@
+
+variable "server_http_port" {
+    description = "The Port for the server"
+    type = number
+    default = 80
+}
+
+variable "region" {
+  description = "region specified here"
+    type = string
+  default = "ap-south-1"
+}
